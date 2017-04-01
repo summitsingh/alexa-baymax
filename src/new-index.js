39eb11e440a7b47cf93e1a1917e2db9c4b4ab34b
Summit Singh Thakur <summitsingh5@gmail.com>
2017-04-01 16:32:45 -0400

    Update new-index.js

--- src/new-index.js
+++ src/new-index.js
@@ -19,8 +19,11 @@ var states = {
     DESCRIPTIONMODE: '_DESCRIPTIONMODE'     // Alexa is describing the final choice and prompting to start again or quit
 };
 
-var academicQuotes = ['Success is not final, failure is not fatal: it is the courage to continue that counts.', 'Failure is simply the opportunity to begin again, this time more intelligently.'];  // Array of items
-var financeQuotes = ['wealth is not everything.', 'money is the root of evil.'];
+var academicsQuotes = ['Success is not final, failure is not fatal: it is the courage to continue that counts.', 'Failure is simply the opportunity to begin again, this time more intelligently.'];  // Array of items
+var relationshipsQuotes = ['relationship quote 1', 'relationship quote 2'];
+var financeQuotes = ['finance quote 1', 'finance quote 2'];
+var workQuotes = ['work quote 1', 'work quote 2'];
+var otherQuotes = ['other quote 1', 'other quote 2'];
 // --------------- Handlers -----------------------
 
 // Called when the session starts.
@@ -30,7 +33,7 @@ exports.handler = function (event, context, callback) {
     alexa.execute();
 };
 
-var welcomeMessage = "I'm sorry to hear that. Do you want to talk about it?";
+var welcomeMessage = "I'm sorry to hear that. You are not alone, and it does get better. Do you want to talk about it?";
 var reason = "";
 var repeatWelcomeMessage = "I'm sorry I didn't understand that. Say yes so that maybe I can help you.";
 var promptToStartMessage = "Are you still here?";
@@ -49,7 +52,7 @@ var newSessionHandler = {
    // this.handler.state = states.STARTMODE;
     var decision= this.event.request.intent.slots.Answer.value;
        if(decision === 'yes'||decision === 'yeah'){
-           this.emit(':tell', 'You are not alone, and it does get better. ' + randomPhrase());
+           this.emit(':tell', 'Ok, I am listening. Tell me a word or sentence that describes how you feel.');
        }else if(decision === 'no'||decision === 'nah'){
            this.emit(':tell', 'That\'s alright, but remember I am here if you need me. Also, you can call 9-1-1')
        }
@@ -57,43 +60,46 @@ var newSessionHandler = {
   'Academics': function () {
    // this.handler.state = states.STARTMODE;
     reason = this.event.request.intent.slots.academics_slot.value;
-    welcomeMessage  = "I'm sorry to hear about your"+reason+". I have a few things in mind to make you feel better. The first is some words that I think will inspire you.";
-    welcomeMessage+=randomPhrase(academicQuotes);
+    welcomeMessage  = "I'm sorry to hear about your issue related to academics. However, I have a few things in mind to make you feel better. The first thing is some words that I think will inspire you.";
+    welcomeMessage+=randomPhrase(academicsQuotes);
     this.emit(':ask',welcomeMessage);
    },
   'Realtionships': function () {
    // this.handler.state = states.STARTMODE;
     reason = this.event.request.intent.slots.realtionships_slot.value;
-    welcomeMessage  = "I'm sorry to hear about your"+reason+". I have a few things in mind to make you feel better. The first is some words that I think will inspire you.";
-    welcomeMessage+=randomPhrase(academicQuotes);
+    welcomeMessage  = "I'm sorry to hear about your issue related to relationships. However, I have a few things in mind to make you feel better. The first thing is some words that I think will inspire you.";
+    welcomeMessage+=randomPhrase(relationshipsQuotes);
     this.emit(':ask',welcomeMessage);
    },
   'Finance': function () {
    // this.handler.state = states.STARTMODE;
     reason = this.event.request.intent.slots.finance_slot.value;
-    welcomeMessage  = "I'm sorry to hear about your"+reason+". I have a few things in mind to make you feel better. The first is some words that I think will inspire you.";
-    welcomeMessage+=randomPhrase(academicQuotes);
+    welcomeMessage  = "I'm sorry to hear about your issue related to finance. However, I have a few things in mind to make you feel better. The first thing is some words that I think will inspire you.";
+    welcomeMessage+=randomPhrase(financeQuotes);
     this.emit(':ask',welcomeMessage);
    },
   'Work': function () {
    // this.handler.state = states.STARTMODE;
     reason = this.event.request.intent.slots.work_slot.value;
-    welcomeMessage  = "I'm sorry to hear about your"+reason+". I have a few things in mind to make you feel better. The first is some words that I think will inspire you.";
-    welcomeMessage+=randomPhrase(academicQuotes);
+    welcomeMessage  = "I'm sorry to hear about your issue related to work. However, I have a few things in mind to make you feel better. The first thing is some words that I think will inspire you.";
+    welcomeMessage+=randomPhrase(workQuotes);
     this.emit(':ask',welcomeMessage);
    },
   'Other': function () {
    // this.handler.state = states.STARTMODE;
     reason = this.event.request.intent.slots.other_slot.value;
-    welcomeMessage  = "I'm sorry to hear about your"+reason+". I have a few things in mind to make you feel better. The first is some words that I think will inspire you.";
-    welcomeMessage+=randomPhrase(academicQuotes);
+    welcomeMessage  = "I'm sorry to hear that. I do not have specific advice for your situation. However, I have a few things in mind to make you feel better. The first thing is some words that I think will inspire you.";
+    welcomeMessage+=randomPhrase(otherQuotes);
     this.emit(':ask',welcomeMessage);
    }
   },
   'Unhandled': function () {
   //  this.handler.state = states.STARTMODE;
     //this.emit(':ask', helpMessage, helpMessage);
-    this.emit(':ask', repeatWelcomeMessage, repeatWelcomeMessage);
+    welcomeMessage  = "I'm sorry to hear that. I do not have specific advice for your situation. However, I have a few things in mind to make you feel better. The first thing is some words that I think will inspire you.";
+    welcomeMessage+=randomPhrase(otherQuotes);
+    this.emit(':ask',welcomeMessage);
+    //this.emit(':ask', repeatWelcomeMessage, repeatWelcomeMessage);
   },
 };
 
