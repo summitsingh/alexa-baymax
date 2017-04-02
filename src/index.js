2338de6a50572935078ee9eb3013f4a6e92f9513
Summit Singh Thakur <summitsingh5@gmail.com>
2017-04-02 01:07:33 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 /*
-Creating Alexa skill in Node.js to enable Alexa to empathize with and
-help a user who is not feeling well or one who displays signs of depression.
-Capabilities include: probing user for cause of low mood, playing soothing music,
-reciting inspirational quote. Will give option later to call loved ones and/or suicide prevention services.
+Creating Alexa skill in Node.js to enable Alexa to empathize with and help a user who is feeling low.
+Capabilities include: probing user for cause of low mood, playing soothing music, reciting inspirational quote.
+Will give option later to call loved ones and/or suicide prevention services.
 
 Ideated and implemented at HackPrinceton, 2017.
 
